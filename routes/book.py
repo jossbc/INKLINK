62afdcbb9f216ed05d13b58af2bc2fe526de8c75
jossbc <contrerasjosslol@gmail.com>
2025-08-14 01:47:33 -0600

    changes in route

--- routes/book.py
+++ routes/book.py
@@ -53,12 +53,12 @@ async def endpoint_books_by_year():
         stats = list(books_coll.aggregate(pipeline))
         return stats
 
-@router.post("/", response_model=Book)
+@router.post("", response_model=Book)
 @validateadmin
 async def create_book_route(request:Request,  book: Book):
     return await create_book(book)
 
-@router.get("/", response_model=list[Book])
+@router.get("", response_model=list[Book])
 async def get_books_route():
     return await get_all_books()
 
